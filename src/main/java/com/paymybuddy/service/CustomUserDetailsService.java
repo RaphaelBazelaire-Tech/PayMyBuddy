79cd1846e48583f231ebbdcffabf593a18983a19
Kyorisan <contact.kyorisan@gmail.com>
2026-04-16 11:04:48 +0200

    Adding CustomUserDetailsService to load user by Spring Security

---
+++ src/main/java/com/paymybuddy/service/CustomUserDetailsService.java
@@ -0,0 +1,31 @@
+package com.paymybuddy.service;
+
+import com.paymybuddy.model.User;
+import com.paymybuddy.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class CustomUserDetailsService implements UserDetailsService {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
+        User user = userRepository.findByEmail(email)
+                .orElseThrow(() -> new UsernameNotFoundException("Aucun utilisateur trouvé avec l'email : " + email));
+
+        return new org.springframework.security.core.userdetails.User(
+                user.getEmail(),
+                user.getPassword(),
+                List.of(new SimpleGrantedAuthority("ROLE_USER"))
+        );
+    }
+}
